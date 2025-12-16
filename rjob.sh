c0cbc179f29762c1df7d91de27f837e984f813df
LspongebobJH <lspongebobjh@gmail.com>
2025-12-16 12:34:37 +0800

    update rjob

---
+++ rjob.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rjob submit \
+--enable-sshd \
+--name=esen-cons-balanced \
+--gpu=8 \
+--memory=128000 \
+--cpu=64 \
+--charged-group=omnimat_gpu \
+--private-machine=group \
+--mount=gpfs://gpfs1/$(whoami):/mnt/shared-storage-user/$(whoami) \
+--image=registry.h.pjlab.org.cn/ailab-omnimat/chenshuizhou-workspace:20250917184047 \
+-P 2 \
+--host-network=true \
+--preemptible=no \
+-e DISTRIBUTED_JOB=true \
+--custom-resources rdma/mlnx_shared=8 \
+--positive-tags node/gpu-l-lg-cmc-h-h200-0230.host.h.pjlab.org.cn,node/gpu-l-lg-cmc-h-h200-0283.host.h.pjlab.org.cn \
+-- bash -exc /mnt/shared-storage-user/$(whoami)/jobs/esen-clean/run_2node16gpu.sh
